b32e4a18025d36ade362eeb3bf61136268a1028c
gouveiarocha <gouveia.rocha@hotmail.com>
2022-03-09 17:48:14 -0300

    add study codes, add module unifatecie, add adjusts codes

---
+++ Unifatecie/src/POO/Teste.java
@@ -0,0 +1,30 @@
+package POO;
+
+import POO.model.ContaCorrente;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        ContaCorrente contaCorrente = new ContaCorrente("Douglas Gouveia", 555611, 0055);
+
+        contaCorrente.depositar(1555.22); //deposito ok
+        contaCorrente.depositar(100.24); //deposito ok
+
+        contaCorrente.sacar(10000.10); //erro ao sacar
+
+        //simulando saque utilizando o retorno proposto
+        double valor = 8000.15;
+        if (contaCorrente.sacar(valor) == 1){
+            //sucesso ao sacar
+        } else if (contaCorrente.sacar(valor) == 0){
+            //erro ao sacara
+        }
+
+        contaCorrente.sacar(1000); //sacar ok
+
+        contaCorrente.imprimir();
+
+    }
+
+}
