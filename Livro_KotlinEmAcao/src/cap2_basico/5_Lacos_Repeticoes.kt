8fea71e60f067f98ad6e5a525c488a256bd7f024
gouveiarocha <gouveia.rocha@hotmail.com>
2022-05-13 11:09:04 -0300

    add study pagina 74

--- Livro_KotlinEmAcao/src/cap2_basico/5_Lacos_Repeticoes.kt
+++ Livro_KotlinEmAcao/src/cap2_basico/5_Lacos_Repeticoes.kt
@@ -1,6 +1,6 @@
 package cap2_basico
 
-import java.util.TreeMap
+import java.util.*
 
 /**
  * Laços de repetição.
@@ -40,11 +40,11 @@ fun main() {
 
     // iterando por um mapa - exp pag 72
     val binaryReps = TreeMap<Char, String>()
-    for (c in 'A'..'F'){
+    for (c in 'A'..'F') {
         val binary = Integer.toBinaryString(c.toInt())
         binaryReps[c] = binary
     }
-    for ((letter, binary) in binaryReps){
+    for ((letter, binary) in binaryReps) {
         println("$letter = $binary")
     }
 
@@ -52,11 +52,33 @@ fun main() {
 
     // exemplo onde não é necessário criar uma variavel separada para armazenar o indice e incrementa-lo manualmente
     val list = arrayListOf("10", "11", "1001")
-    for ((index, element) in list.withIndex()){
+    for ((index, element) in list.withIndex()) {
         println("$index: $element")
     }
 
+    println("---------------")
+
+    //usando o in para verificar se um determinado caractere pertence a um intervalo
+    println(isLetter('b'))
+    println(isNotDigit('5'))
+
+    //usando verificação com in em ramos de when
+    println(recognize('@'))
+
+    //verificando se outro objeto pertence ao intervalo usando o in
+    println("Kotlin" in "Scala".."Java") //true
+    println("Kotlin" in "Java".."Scala") //false
+
 }
+
+fun recognize(caracter: Char) = when (caracter) {
+    in '0'..'9' -> "It's a digit!"
+    in 'a'..'z', in 'A'..'Z' -> "It's a letter!"
+    else -> "I don't know..."
+}
+
+fun isLetter(caracter: Char) = caracter in 'a'..'z' || caracter in 'A'..'Z'
+fun isNotDigit(caracter: Char) = caracter !in '0'..'9'
 
 fun lacosWhile() {
 
