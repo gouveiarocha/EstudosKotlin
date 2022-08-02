924733a6e568262ba3c2dec0be3ed597f42f5293
gouveiarocha <gouveia.rocha@hotmail.com>
2022-08-02 16:40:54 -0300

    Finalização do Capitulo 2 e Resumo.

--- Livro_KotlinEmAcao/src/cap2_basico/6_Excessoes.kt
+++ Livro_KotlinEmAcao/src/cap2_basico/6_Excessoes.kt
@@ -2,21 +2,35 @@ package cap2_basico
 
 import java.io.BufferedReader
 import java.io.StringReader
-import java.lang.IllegalArgumentException
-import java.lang.NumberFormatException
 
 /**
  * Laços de repetição.
- * Pag. 75 a...
+ * Pag. 75 a 78.
  */
 
 fun main() {
 
     //teste1_pag75()
 
-    //exemplo usando try, catch e finally
-    teste2_pag76()
+    //teste2_pag76() //exemplo usando try, catch e finally
 
+    //Observação importante para os exemplos abaixo. A última expressão do bloco(try ou catch) é que será o resultado.
+    //exemplo usando o try como uma expressão...
+    teste3_pag77()
+    //mesmo exemplo do anterior, mas observar que podemos retornar um valor no catch, isso é bomo para os casos
+    // que queremos continuar a execução e não utilizar o return. Lembrando, somente a ultima expressão será o resultado.
+    teste4_pag78()
+
+}
+
+private fun teste4_pag78() {
+    val reader = BufferedReader(StringReader("not a number"))
+    readNumber3(reader)
+}
+
+private fun teste3_pag77() {
+    val reader = BufferedReader(StringReader("not a number"))
+    readNumber2(reader)
 }
 
 private fun teste2_pag76() {
@@ -34,18 +48,41 @@ private fun teste1_pag75() {
     val percentage2 = if (number in 0..100) number else throw IllegalArgumentException("msg erro")
 }
 
-fun readNumber(reader: BufferedReader):Int? {
+/**
+ * Métodos
+ */
 
+fun readNumber(reader: BufferedReader): Int? {
     try {
         //recebe um dado
         val line = reader.readLine()
         //tentar converter e retornar se sucesso
         return Integer.parseInt(line)
-    }catch (e: NumberFormatException){
+    } catch (e: NumberFormatException) {
         //lança excessão e retornar null
         return null
-    }finally {
+    } finally {
         //sempre executa
         reader.close()
     }
+}
+
+fun readNumber2(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        return
+    }
+    println(number)
+}
+
+fun readNumber3(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        null
+        //continua a execução...
+        println("continuei executando...")
+    }
+    println(number)
 }
